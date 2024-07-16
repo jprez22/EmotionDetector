d3939303c73c6ab4ae068679e58d8d1147ab3c99
jprez22 <jpreziuso14@gmail.com>
2024-07-16 15:12:29 -0400

    Added emotion_predictor and ran some tests

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -19,4 +19,26 @@ def emotion_detector(text_to_analyze):
                             'sadness': None, 
                             'dominant_emotion': None}
         return formated_response
+
+def emotion_predictor(detected_text):
+    if all(value is None for value in detected_text.values()):
+        return detected_text
+    if detected_text['emotionPredictions'] is not None:
+        emotions = detected_text['emotionPredictions'][0]['emotion']
+        anger = emotions['anger']
+        disgust = emotions['disgust']
+        fear = emotions['fear']
+        joy = emotions['joy']
+        sadness = emotions['sadness']
+        max_emotion = max(emotions, key=emotions.get)
+        #max_emotion_score = emotions[max_emotion]
+        formated_dict_emotions = {
+                                'anger': anger,
+                                'disgust': disgust,
+                                'fear': fear,
+                                'joy': joy,
+                                'sadness': sadness,
+                                'dominant_emotion': max_emotion
+                                }
+        return formated_dict_emotions
     
